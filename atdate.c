c44584e9c86b73f5c7ddc9ee192612292aade49a
RyuS3ki <rbe.jz.gn@gmail.com>
2018-03-17 12:06:38 +0100

    License

--- atdate.c
+++ atdate.c
@@ -1,3 +1,20 @@
+/*   NTP Client - Server
+ *   Copyright (C) 2017 Rebeca Jiménez Guillén
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -405,7 +422,7 @@ int main(int argc, char* const argv[]) {
   int debug = 0;
   char* mode = NULL;
   char* host = NULL;
-	int port = 37; // Default port for clients
+  int port = 37; // Default port for clients
   char *str_opt;
 
   /* Parsing command-line arguments
